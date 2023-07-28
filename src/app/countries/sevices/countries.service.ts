ea289071b24e3bedabad6869ad645e591f21a0ba
Marc España Quinquilla <meq.1515@gmail.com>
2023-07-28 17:22:11 +0200

    En el servicio creado se han hecho varias cosas que debería haberlo repartido en diferentes commits, como sea, lo que he hecho es guardar la url principal en una variable para que se pueda comportar de forma dinámica. Con un getter permitiremos obtener las regiones en el componente SelectorPageComponent. Con el observable getCountriesByRegion tipamos para que devuelva un SmallCountry[], con el operador of en el caso de que no hubiera datos devolvemos un array vacío para manejar de mejor forma el posible error. Con la variable url creamos la petición dinámica que le vamos a pasar al observable Country[] que deberá devolver. Mediante un map convertimos los datos a nuestro placer respetando el tipado y finalmente los datos obtenidos se envían.

---
+++ src/app/countries/sevices/countries.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, map, of, tap } from 'rxjs';
+
+import { Country, Region, SmallCountry } from '../interfaces/countries.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CountriesService {
+
+  private baseUrl: string = 'https://restcountries.com/v3.1'
+
+
+  private _regions: Region[] = [ Region.Africa, Region.Americas, Region.Asia, Region.Europa, Region.Oceania ];
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+
+  get regions(): Region[] {
+    return [...this._regions];
+  }
+
+  getCountriesByRegion( region: Region ): Observable<SmallCountry[]> {
+
+    if ( !region ) return of ([]);
+
+    const url: string = `${ this.baseUrl }/region/${ region }?fields=cca3,name,borders`;
+
+    return this.http.get<Country[]>(url)
+      .pipe(
+        map( countries => countries.map( country => ({
+          name: country.name.common,
+          cca3: country.cca3,
+          borders: country.borders ?? []
+        }))),
+      );
+
+  }
+
+
+}
